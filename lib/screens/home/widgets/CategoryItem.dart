b8f3084f666e48389bd2a13d7251dcdf78d299f5
sleepy-DA <chanthida.choup@gmail.com>
2026-01-23 13:18:53 +0700

    Clean CategoryItem UI and resolve lint warnings

--- lib/screens/home/widgets/CategoryItem.dart
+++ lib/screens/home/widgets/CategoryItem.dart
@@ -1,12 +1,14 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_news_new/common/colors.dart';
 
-class CategoryItem extends StatefulWidget {
+class CategoryItem extends StatelessWidget {
   final String categoryName;
   final int index;
   final int activeCategory;
-  final Function onClick;
+  final VoidCallback onClick;
 
   const CategoryItem({
     super.key,
@@ -17,50 +19,38 @@ class CategoryItem extends StatefulWidget {
   });
 
   @override
-  State<CategoryItem> createState() => _CategoryItemState();
-}
-
-class _CategoryItemState extends State<CategoryItem> {
-  @override
   Widget build(BuildContext context) {
-    final bool isActive = widget.activeCategory == widget.index;
+    final bool isActive = activeCategory == index;
 
-    return GestureDetector(
-      onTap: () => widget.onClick(),
-      child: Container(
-        width: 130,
-        margin: const EdgeInsets.symmetric(horizontal: 5),
-        alignment: Alignment.center,
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-        decoration: BoxDecoration(
-          color: isActive
-              ? AppColors.primary.withOpacity(0.1) // highlight active
-              : Colors.transparent,
-          border: Border.all(
-            color: isActive ? AppColors.black : AppColors.lighterBlack,
-            width: isActive ? 2 : 1,
-          ),
-          borderRadius: BorderRadius.circular(50),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              widget.categoryName.isNotEmpty
-                  ? '${widget.categoryName[0].toUpperCase()}${widget.categoryName.substring(1)}'
-                  : '',
-              style: GoogleFonts.poppins(
-                textStyle: TextStyle(
-                  color: isActive ? AppColors.black : AppColors.lighterBlack,
-                  fontWeight: isActive ? FontWeight.w600 : FontWeight.w500,
-                ),
-              ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(50),
+        onTap: onClick,
+        child: Container(
+          width: 130,
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: isActive
+                ? AppColors.primary.withValues(alpha: 0.08)
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(50),
+            border: Border.all(
+              color: isActive ? AppColors.primary : AppColors.lighterBlack,
+              width: isActive ? 1.5 : 1,
             ),
-            if (isActive) ...[
-              const SizedBox(width: 4),
-              const Icon(Icons.star, size: 14, color: Colors.orangeAccent), // Thida marker
-            ]
-          ],
+          ),
+          child: Text(
+            categoryName.isNotEmpty
+                ? '${categoryName[0].toUpperCase()}${categoryName.substring(1)}'
+                : '',
+            style: GoogleFonts.poppins(
+              fontSize: 14,
+              fontWeight: isActive ? FontWeight.w600 : FontWeight.w500,
+              color: isActive ? AppColors.primary : AppColors.lighterBlack,
+            ),
+          ),
         ),
       ),
     );
